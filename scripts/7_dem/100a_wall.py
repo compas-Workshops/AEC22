ce1a28afb1579f1c536d171464089aa6c2b79b5f
brgcode <tom.v.mele@gmail.com>
2022-10-12 08:02:28 +0200

    assembly and interfaces

--- scripts/7_dem/100a_wall.py
+++ scripts/7_dem/100a_wall.py
@@ -1,10 +1,19 @@
-from compas.geometry import Point, Plane, Frame
+from typing import List
+
+from compas.geometry import Point, Plane, Frame, Line
 from compas.geometry import NurbsCurve
 from compas.geometry import Box
 from compas.geometry import Transformation
 from compas.geometry import Translation
+from compas.geometry import Polygon
+
 from compas.colors import Color
 from compas.utilities import pairwise
+
+from compas_assembly.datastructures import Assembly
+from compas_assembly.datastructures import Block
+from compas_assembly.datastructures import Interface
+from compas_assembly.algorithms import assembly_interfaces
 
 from compas_occ.geometry import OCCSurface
 from compas_occ.conversions import compas_plane_to_occ_plane
@@ -22,9 +31,9 @@ from compas_view2.objects import Collection
 
 points = [
     Point(0, 0, 0),
-    Point(3, 3, 0),
-    Point(6, -6, 0),
-    Point(9, 0, 0),
+    Point(2, 2, 0),
+    Point(4, -4, 0),
+    Point(6, 0, 0),
 ]
 
 curve = NurbsCurve.from_points(points)
@@ -35,7 +44,7 @@ span = curve.domain[1] - curve.domain[0]
 # Params
 # =============================================================================
 
-N = 20
+N = 11
 W = 0.4
 H = 0.2
 
@@ -138,7 +147,7 @@ for box, (u, v) in zip(odd, pairwise(oddparams)):
 
 courses = []
 
-for i in range(10):
+for i in range(6):
     course = []
     if i % 2:
         boxes = odd
@@ -148,6 +157,19 @@ for i in range(10):
     for box in boxes:
         course.append(box.transformed(translation))
     courses.append(course)
+
+# =============================================================================
+# Assembly
+# =============================================================================
+
+assembly = Assembly()
+
+for course in courses:
+    for box in course:
+        block = Block.from_shape(box)
+        assembly.add_block(block)
+
+assembly_interfaces(assembly, tmax=1e-6, amin=1e-2)
 
 # =============================================================================
 # Viz
@@ -160,7 +182,7 @@ viewer.add(outer.to_polyline(), linecolor=(1, 0, 0))
 viewer.add(inner.to_polyline(), linecolor=(0, 0, 1))
 
 for frame in frames:
-    viewer.add(frame, linewidth=5, size=0.5)
+    viewer.add(frame, linewidth=5, size=0.1)
 
 viewer.add(Collection(outerpoints))
 viewer.add(Collection(innerpoints))
@@ -172,8 +194,33 @@ viewer.add(Collection(innerpoints))
 #     box.transform(Translation.from_vector([0, 0, box.zsize]))
 #     viewer.add(box, facecolor=Color.red().lightened(75), linecolor=Color.red())
 
-for course in courses:
-    for box in course:
-        viewer.add(box, facecolor=Color.grey().lightened(75), linecolor=Color.grey())
+# for course in courses:
+#     for box in course:
+#         viewer.add(box, facecolor=Color.grey().lightened(75), linecolor=Color.grey())
+
+for node in assembly.graph.nodes():
+    block = assembly.graph.node_attribute(node, "block")
+
+    if assembly.graph.node_attribute(node, "is_support"):
+        color = Color.red()
+    else:
+        color = Color.grey()
+
+    viewer.add(block, facecolor=color.lightened(50), linecolor=color, linewidth=2, opacity=0.5, show_faces=False)
+
+    viewer.add(Point(*block.centroid()))
+
+for edge in assembly.graph.edges():
+    interfaces: List[Interface] = assembly.graph.edge_attribute(edge, "interfaces")
+    for interface in interfaces:
+        polygon = Polygon(interface.points)
+        viewer.add(polygon)
+
+    a = assembly.graph.node_attribute(edge[0], "block").centroid()
+    b = assembly.graph.node_attribute(edge[1], "block").centroid()
+
+    line = Line(a, b)
+
+    viewer.add(line, linewidth=3)
 
 viewer.run()
